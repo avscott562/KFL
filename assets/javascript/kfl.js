ee8aac05b8cf6896f612dd4553bb0e936b425a4e
Scottie <avscott@pacbell.net>
2019-09-14 12:55:50 -0700

    add script tags to html & initializing api info in js

--- assets/javascript/kfl.js
+++ assets/javascript/kfl.js
@@ -1,3 +1,15 @@
-var teams = ["Arizona Cardinals", "Atlanta Falcons", "Baltimore Ravens", "Buffalo Bills", "Carolina Panthers", "Chicago Bears", "Cincinnati Bengals", "Cleveland Browns", "Dallas Cowboys", "Denver Broncos", "Detroit Lions", "Green Bay Packers", "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars", "Kansas City Chiefs", "Los Angeles Chargers", "Los Angeles Rams", "Miami Dolphins", "Minnesota Vikings", "New England Patriots", "New Orleans Saints", "New York Gians", "New York Jets", "Oakland Raiders", "Philadelphia Eagles", "Pittsburgh Steelers", "San Franciso 49ers", "Seattle Seahawks", "Tampa Bay Buccaneers", "Tennessee Titans", "Washington Redskins"];
+let client = new JSO({
+	providerID: "google",
+	client_id: "541950296471.apps.googleusercontent.com",
+	redirect_uri: "http://localhost:8080/", // The URL where you is redirected back, and where you perform run the callback() function. The URI that the user will be redirected back to when completed. This should be the same URL that the page is presented on.
+	authorization: "https://accounts.google.com/o/oauth2/auth",
+	scopes: { request: ["https://www.googleapis.com/auth/userinfo.profile"]}
+})
 
-var quarterbacks = [""];
+// var teams = ["Arizona Cardinals", "Atlanta Falcons", "Baltimore Ravens", "Buffalo Bills", "Carolina Panthers", "Chicago Bears", "Cincinnati Bengals", "Cleveland Browns", "Dallas Cowboys", "Denver Broncos", "Detroit Lions", "Green Bay Packers", "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars", "Kansas City Chiefs", "Los Angeles Chargers", "Los Angeles Rams", "Miami Dolphins", "Minnesota Vikings", "New England Patriots", "New Orleans Saints", "New York Gians", "New York Jets", "Oakland Raiders", "Philadelphia Eagles", "Pittsburgh Steelers", "San Franciso 49ers", "Seattle Seahawks", "Tampa Bay Buccaneers", "Tennessee Titans", "Washington Redskins"];
+
+// var quarterbacks = ["Brett Hundley", "Kyler Murray"];
+
+// var runningBacks = ["D.J. Foster", "Chase Edmonds", "David Johnson"];
+
+// var kickers = ["Zane Gonzales", ];
