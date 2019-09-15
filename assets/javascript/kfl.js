b62913b7790fcbd4a0bf46a67897ee5f176c7e80
Scottie <avscott@pacbell.net>
2019-09-15 09:56:04 -0700

    add notes for stats

--- assets/javascript/kfl.js
+++ assets/javascript/kfl.js
@@ -6,6 +6,8 @@ let client = new JSO({
 	scopes: { request: ["https://www.googleapis.com/auth/userinfo.profile"]}
 })
 
+//5 positions needed - quarterback QB, wide receiver WR, running back RB, kicker K, tight end TE
+
 // var teams = ["Arizona Cardinals", "Atlanta Falcons", "Baltimore Ravens", "Buffalo Bills", "Carolina Panthers", "Chicago Bears", "Cincinnati Bengals", "Cleveland Browns", "Dallas Cowboys", "Denver Broncos", "Detroit Lions", "Green Bay Packers", "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars", "Kansas City Chiefs", "Los Angeles Chargers", "Los Angeles Rams", "Miami Dolphins", "Minnesota Vikings", "New England Patriots", "New Orleans Saints", "New York Gians", "New York Jets", "Oakland Raiders", "Philadelphia Eagles", "Pittsburgh Steelers", "San Franciso 49ers", "Seattle Seahawks", "Tampa Bay Buccaneers", "Tennessee Titans", "Washington Redskins"];
 
 // var quarterbacks = ["Brett Hundley", "Kyler Murray"];
