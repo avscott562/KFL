281b01a15a804f4e20c99e9149f55f9166e66737
Scottie <avscott@pacbell.net>
2019-09-18 03:18:26 -0700

    add wiki api call

---
+++ assets/javascript/wiki.js
@@ -0,0 +1,21 @@
+AOS.init();
+
+$('#submit').on("click", function(event) {
+    event.preventDefault();
+    console.log($('#searchItem').val().trim());
+    let searchItem = $('#searchItem').val().trim().replace(" ", "%20");
+
+    let searchURL = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + searchItem;
+
+    $.ajax({
+        url: searchURL,
+        method: "GET"
+    })
+    .then(function(response) {
+        let results = response;
+        let newDiv = $('<div>').html(results[1][0]);
+        let newPara = $('<p>').html(results[2][0]);
+        let newLink = $('<a>').attr("src", results[3][0]);
+        console.log(results);
+    });
+});
