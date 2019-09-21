954e4fb0e795f183643c95780eebbacace41ea45
Scottie <avscott@pacbell.net>
2019-09-20 20:32:18 -0700

    add info to modal

--- assets/javascript/kfl.js
+++ assets/javascript/kfl.js
@@ -1,129 +1,124 @@
 
-//Declare variables
-//array of teams
-let teams = [];
-//array of bye weeks
-let byes = [];
-//5 positions needed - quarterback QB, wide receiver WR, running back RB, kicker K, tight end TE
-//array of quarterbacks
-let quarterbacks = [];
-//array of running backs
-let runningBacks = [];
-//array of wide receivers
-let wideReceivers = [];
-//array of top tight ends
-let tightEnds = [];
-//array of kickers
-let kickers = [];
+// //Declare variables
+// //array of teams
+// let teams = [];
+// //array of bye weeks
+// let byes = [];
+// //5 positions needed - quarterback QB, wide receiver WR, running back RB, kicker K, tight end TE
+// //array of quarterbacks
+// let quarterbacks = [];
+// //array of running backs
+// let runningBacks = [];
+// //array of wide receivers
+// let wideReceivers = [];
+// //array of top tight ends
+// let tightEnds = [];
+// //array of kickers
+// let kickers = [];
 
-jQuery.ajaxPrefilter(function(options) {
-    if (options.crossDomain && jQuery.support.cors) {
-        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-    }
-});
+// jQuery.ajaxPrefilter(function(options) {
+//     if (options.crossDomain && jQuery.support.cors) {
+//         options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+//     }
+// });
 
 
-//grab teams from api
-//variable to hold api query for teams
-let queryTeamsURL = "https://www.fantasyfootballnerd.com/service/nfl-teams/json/test";
-//"https://www.fantasyfootballnerd.com/service/" + searchTerm + "/json/test";
+// //grab teams from api
+// //variable to hold api query for teams
+// let queryTeamsURL = "https://www.fantasyfootballnerd.com/service/nfl-teams/json/test";
+// //"https://www.fantasyfootballnerd.com/service/" + searchTerm + "/json/test";
 
-//pulling data fron the api
-$.ajax({
-	url: queryTeamsURL,
-	method: "GET"
-})
-  //after we get data from the api
-  .then(function(response) {
-	  teams = response.NFLTeams;
-	  console.log(teams);
-});
+// //pulling data fron the api
+// $.ajax({
+// 	url: queryTeamsURL,
+// 	method: "GET"
+// })
+//   //after we get data from the api
+//   .then(function(response) {
+// 	  teams = response.NFLTeams;
+// });
 
-//grab bye weeks from api
-//variable to hold api query for bye weeks
-let queryByesURL = "https://www.fantasyfootballnerd.com/service/byes/json/test";
+// //grab bye weeks from api
+// //variable to hold api query for bye weeks
+// let queryByesURL = "https://www.fantasyfootballnerd.com/service/byes/json/test";
 
-//pulling data fron the api
-$.ajax({
-	url: queryByesURL,
-	method: "GET"
-})
-  //after we get data from the api
-  .then(function(response) {
-	  byes = response;
-});
+// //pulling data fron the api
+// $.ajax({
+// 	url: queryByesURL,
+// 	method: "GET"
+// })
+//   //after we get data from the api
+//   .then(function(response) {
+// 	  byes = response;
+// });
   
-//grab QBs from api
-//variable to hold api query for QBs
-let queryQBURL = "https://www.fantasyfootballnerd.com/service/players/json/test/QB";
+// //grab QBs from api
+// //variable to hold api query for QBs
+// let queryQBURL = "https://www.fantasyfootballnerd.com/service/players/json/test/QB";
 
-//pulling data fron the api
-$.ajax({
-	url: queryQBURL,
-	method: "GET"
-})
-  //after we get data from the api
-  .then(function(response) {
-	  quarterbacks = response.Players;
-	  console.log(quarterbacks);
-});
+// //pulling data fron the api
+// $.ajax({
+// 	url: queryQBURL,
+// 	method: "GET"
+// })
+//   //after we get data from the api
+//   .then(function(response) {
+// 	  quarterbacks = response.Players;
+// });
 
-//grab RBs from api
-//variable to hold api query for RBs
-let queryRBURL = "https://www.fantasyfootballnerd.com/service/players/json/test/RB";
+// //grab RBs from api
+// //variable to hold api query for RBs
+// let queryRBURL = "https://www.fantasyfootballnerd.com/service/players/json/test/RB";
 
-//pulling data fron the api
-$.ajax({
-	url: queryRBURL,
-	method: "GET"
-})
-  //after we get data from the api
-  .then(function(response) {
-	  runningBacks = response.Players;
-	  console.log(runningBacks);
-});
+// //pulling data fron the api
+// $.ajax({
+// 	url: queryRBURL,
+// 	method: "GET"
+// })
+//   //after we get data from the api
+//   .then(function(response) {
+// 	  runningBacks = response.Players;
+// });
 
-//grab WRs from api
-//variable to hold api query for RBs
-let queryWRURL = "https://www.fantasyfootballnerd.com/service/players/json/test/WR";
+// //grab WRs from api
+// //variable to hold api query for RBs
+// let queryWRURL = "https://www.fantasyfootballnerd.com/service/players/json/test/WR";
 
-//pulling data fron the api
-$.ajax({
-	url: queryWRURL,
-	method: "GET"
-})
-  //after we get data from the api
-  .then(function(response) {
-	wideReceivers = response.Players;
-	  console.log(wideReceivers);
-});
+// //pulling data fron the api
+// $.ajax({
+// 	url: queryWRURL,
+// 	method: "GET"
+// })
+//   //after we get data from the api
+//   .then(function(response) {
+// 	wideReceivers = response.Players;
+// });
 
-//grab TEs from api
-//variable to hold api query for RBs
-let queryTEURL = "https://www.fantasyfootballnerd.com/service/players/json/test/TE";
+// //grab TEs from api
+// //variable to hold api query for RBs
+// let queryTEURL = "https://www.fantasyfootballnerd.com/service/players/json/test/TE";
 
-//pulling data fron the api
-$.ajax({
-	url: queryTEURL,
-	method: "GET"
-})
-  //after we get data from the api
-  .then(function(response) {
-	tightEnds = response.Players;
-	  console.log(tightEnds);
-});
+// //pulling data fron the api
+// $.ajax({
+// 	url: queryTEURL,
+// 	method: "GET"
+// })
+//   //after we get data from the api
+//   .then(function(response) {
+// 	tightEnds = response.Players;
+// });
 
-//grab Ks from api
-//variable to hold api query for RBs
-let queryKURL = "https://www.fantasyfootballnerd.com/service/players/json/test/K";
 
-//pulling data fron the api
-$.ajax({
-	url: queryKURL,
-	method: "GET"
-})
-  //after we get data from the api
-  .then(function(response) {
-	kickers = response.Players;
-	  console.log(kickers);
-});
+// //grab Ks from api
+// //variable to hold api query for RBs
+// let queryKURL = "https://www.fantasyfootballnerd.com/service/players/json/test/K";
+
+// //pulling data fron the api
+// $.ajax({
+// 	url: queryKURL,
+// 	method: "GET"
+// })
+//   //after we get data from the api
+//   .then(function(response) {
+// 	kickers = response.Players;
+// });
--- assets/javascript/league.js
+++ assets/javascript/league.js
@@ -1,4 +1,128 @@
+//Declare variables
+//array of teams
+let teams = [];
+//array of bye weeks
+let byes = [];
+//5 positions needed - quarterback QB, wide receiver WR, running back RB, kicker K, tight end TE
+//array of quarterbacks
+let quarterbacks = [];
+//array of running backs
+let runningBacks = [];
+//array of wide receivers
+let wideReceivers = [];
+//array of top tight ends
+let tightEnds = [];
+//array of kickers
+let kickers = [];
 
+jQuery.ajaxPrefilter(function(options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+});
+
+
+//grab teams from api
+//variable to hold api query for teams
+let queryTeamsURL = "https://www.fantasyfootballnerd.com/service/nfl-teams/json/test";
+//"https://www.fantasyfootballnerd.com/service/" + searchTerm + "/json/test";
+
+//pulling data fron the api
+$.ajax({
+	url: queryTeamsURL,
+	method: "GET"
+})
+  //after we get data from the api
+  .then(function(response) {
+	  teams = response.NFLTeams;
+});
+
+//grab bye weeks from api
+//variable to hold api query for bye weeks
+let queryByesURL = "https://www.fantasyfootballnerd.com/service/byes/json/test";
+
+//pulling data fron the api
+$.ajax({
+	url: queryByesURL,
+	method: "GET"
+})
+  //after we get data from the api
+  .then(function(response) {
+	  byes = response;
+});
+  
+//grab QBs from api
+//variable to hold api query for QBs
+let queryQBURL = "https://www.fantasyfootballnerd.com/service/players/json/test/QB";
+
+//pulling data fron the api
+$.ajax({
+	url: queryQBURL,
+	method: "GET"
+})
+  //after we get data from the api
+  .then(function(response) {
+	  quarterbacks = response.Players;
+});
+
+//grab RBs from api
+//variable to hold api query for RBs
+let queryRBURL = "https://www.fantasyfootballnerd.com/service/players/json/test/RB";
+
+//pulling data fron the api
+$.ajax({
+	url: queryRBURL,
+	method: "GET"
+})
+  //after we get data from the api
+  .then(function(response) {
+	  runningBacks = response.Players;
+});
+
+//grab WRs from api
+//variable to hold api query for RBs
+let queryWRURL = "https://www.fantasyfootballnerd.com/service/players/json/test/WR";
+
+//pulling data fron the api
+$.ajax({
+	url: queryWRURL,
+	method: "GET"
+})
+  //after we get data from the api
+  .then(function(response) {
+	wideReceivers = response.Players;
+});
+
+//grab TEs from api
+//variable to hold api query for RBs
+let queryTEURL = "https://www.fantasyfootballnerd.com/service/players/json/test/TE";
+
+//pulling data fron the api
+$.ajax({
+	url: queryTEURL,
+	method: "GET"
+})
+  //after we get data from the api
+  .then(function(response) {
+	tightEnds = response.Players;
+});
+
+
+//grab Ks from api
+//variable to hold api query for RBs
+let queryKURL = "https://www.fantasyfootballnerd.com/service/players/json/test/K";
+
+//pulling data fron the api
+$.ajax({
+	url: queryKURL,
+	method: "GET"
+})
+  //after we get data from the api
+  .then(function(response) {
+	kickers = response.Players;
+});
+
+  
   var leagues = [
     {
       name: "Football Heroes",
@@ -126,7 +250,6 @@
       });
       newP.append(newA);
       $('.leagueList').append(newP);
-      console.log(leagues[i].name);
     }
   }
   
@@ -140,6 +263,7 @@
     event.preventDefault();
     let lName = $(this).attr("data-name");
     let desc = $(this).attr("data-description");
+    $('#leagueNameModal').attr("data-name", lName).text(lName);
     $('#leagueNameTitle').text(lName);
     $('#leagueDescription').text(desc);
     $('#memberList').addClass('show');
@@ -190,3 +314,16 @@
       }
     }
   });
+
+  $('#joinNow').on("click", function () {
+   
+  });
+
+  function CreateTeam(league, qb, wr, te, k, rb) {
+    this.league = league;
+    this.qb = qb;
+    this.wr = wr;
+    this.te = te;
+    this.k = k;
+    this.rb = rb;
+  }
